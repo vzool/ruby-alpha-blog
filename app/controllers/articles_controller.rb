49b53595a16e006f81c17d8c219fe3e37a5a58ac
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2015-12-18 18:57:18 +0000

    add show data by id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,31 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
   def create
     # render plain: params[:article].inspect
+
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was Successfully Created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
-  private def article_params
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
     params.require(:article).permit(:title, :description)
   end
 
